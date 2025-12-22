020240cbf9383b08a12505f38e2f58d41dd34f12
Kompotie <129387960+Kompotie@users.noreply.github.com>
2025-12-22 22:24:20 +0500

    Delete frontend directory

--- frontend/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.game.gameguessnumber {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.game.gameguessnumber to javafx.fxml;
-    exports com.game.gameguessnumber;
-}
